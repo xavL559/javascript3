6fb181e02d98a8835b405f1d40ea7a4bfd3521a2
Xavier Leftridge <xavierleftridge7@gmail.com>
2022-11-07 19:27:22 -0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,12 @@
+// 1. Write code to display a triangle in the console that looks similar to the following.
+// You should use a loop to accomplish this.
+// #
+// ##
+// ###
+// ####
+// ###
+// ##
+// #
+
+
+for(let triangle = "0";  triangle.length <= 7; triangle)
