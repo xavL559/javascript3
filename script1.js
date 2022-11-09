7ee171a222dcb1aa50bf17df4b44ed64da379945
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2022-11-08 22:09:55 -0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,12 +1,5 @@
-// 1. Write code to display a triangle in the console that looks similar to the following.
-// You should use a loop to accomplish this.
-// #
-// ##
-// ###
-// ####
-// ###
-// ##
-// #
+for(let triangle = "#"; triangle.length <= 7; triangle += "#")
+
+console.log(triangle);
 
 
-for(let triangle = "0";  triangle.length <= 7; triangle)
