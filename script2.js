5c3361130736de8aca49f79a7b6daa706d2da261
Xavier Leftridge <111714709+xavL559@users.noreply.github.com>
2022-11-08 22:09:57 -0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -9,3 +9,23 @@
 // 14
 // FizzBuzz
 // ...
+function fizzbuzz(number){
+    for(var i = 1; i < number ; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }
+        else if(i % 3 === 0){
+            console.log("Fizz");
+        }
+        else if(i % 5 === 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log("Vaule of i is " + i);
+        }
+    }
+}
+
+var test =100;
+
+fizzbuzz(test);
